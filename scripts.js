32a4744c5b718aee5df99873cabb19ff378ae263
christine <christine.clark@uleth.ca>
2023-10-31 13:49:29 -0600

    added assets

--- scripts.js
+++ scripts.js
@@ -12,10 +12,26 @@ ulethLogo.replaceWith(updateLogo);
 const brandheader = document.getElementsByClassName("brand-header");
 console.log(brandheader[0]);
 brandheader[0].textContent = "New Media 4 Life";
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log(request.message)
         document.body.style.backgroundColor = request.message;
+
+        let newbkg = chrome.runtime.getURL("images/lookout.png")
+        document.getElementsByClassName("hero-main")[0].style.backgroundImage = `url(${newbkg})`;
+
+        const newImage = document.createElement("img");
+        newImage.src = chrome.runtime.getURL("images/ilovenmed.png");
+        newImage.classList = "new-image";
+
+
+        var allHTML = document.body.innerHTML;
+        document.body.innerHTML = ""
+        document.body.appendChild(newImage);
+        document.body.innerHTML += allHTML;
+
+
         sendResponse({ message: "Message received" })
     }
 );
